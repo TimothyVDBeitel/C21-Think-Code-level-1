52a46472593709b1d7b439b2f8c648be8bc6c9a5
Timothy van den Beitel <2157002@talnet.nl>
2021-11-15 00:18:11 +0100

    final

---
+++ Eindopdracht/Eindopdracht.py
@@ -0,0 +1,26 @@
+#ik ga het gsprek langer maken.
+print("Hi!")
+name = input("What's your name? ")
+
+print("It's nice to meet you,", name)
+answer = input("Are you enjoying the course? ")
+
+if answer == "Yes":
+    print("That's good to hear!")
+if answer == "No":
+    print("Oh no! That makes me sad!")
+
+
+answer = input("Did you learn anything new lately?")
+
+if answer == "Yes":
+    print("Very good.")
+if answer == "No":
+    print("That's no fun, try something new.")
+
+answer = input("what are you doing right now?")
+
+if answer == "Nothing":
+    print("That is okay")
+else:
+    print("That is interesting to hear",name,"!")
